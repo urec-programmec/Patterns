b0e1551ab24db458f2e9d70b697dca2c362222dd
urec-programmec <sepa33126@gmail.com>
2019-11-01 12:08:29 +0300

    new

---
+++ ConsoleApplication1/ConsoleApplication1/Line.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication1
+{    
+    class Line : ILine
+    {
+        string body = "";
+        string endLeft = "";
+        string endRight = "";
+
+        public Line(int len, EndOfLines left, EndOfLines right)
+        {
+            this.body = new string('-', len);
+            this.endLeft = left.L();
+            this.endRight = right.R();
+        }
+
+        public void changeLeft(EndOfLines newEnd)
+        {
+            this.endLeft = newEnd.L();
+        }
+
+        public void changeRight(EndOfLines newEnd)
+        {
+            this.endLeft = newEnd.R();
+        }
+
+        public string Draw()
+        {
+            return endLeft + body + endRight;
+        }
+    }
+}
